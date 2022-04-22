47c5eab9443389f464593d7b20adf0e0b8e1b3d8
Nezort11 <nezort11@gmail.com>
2022-04-22 22:13:47 +0300

    Add dockerfile

--- {{cookiecutter.project_slug}}/src/client/constants.js
+++ {{cookiecutter.project_slug}}/src/client/constants.js
@@ -1,13 +1,8 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 const urls = {
   // Authentication, registration, account management
-  login: () => `auth/login/`,
-  register: () => `auth/register/`,
-  user: () => `auth/user/`,
+  login: apiBaseUrl + `auth/login/`,
+  register: apiBaseUrl + `auth/register/`,
+  user: apiBaseUrl + `auth/user/`,
 };
-
-export function reverse(urlName, args) {
-  const url = urls[urlName](args);
-  return API_BASE_URL + url;
-}
